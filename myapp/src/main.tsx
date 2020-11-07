adeb450fb5df3dde0ac32c8816dd5388bd42bbae
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-11-07 23:35:16 +0900

    mod

--- myapp/src/main.tsx
+++ myapp/src/main.tsx
@@ -2,8 +2,33 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
-  render () {
-    return <p> Hello React </p>;
+  render() {
+    const human1 = { name: "Hoge", age: 20 };
+    const human2 = { name: "Fuga", age: 30};
+    return <MyTable humans={[human1, human2]} />;
+  }
+}
+
+type Human = {
+  name: string;
+  age: number;
+}
+
+type MyTableProps = {
+  humans: Human[];
+}
+
+class MyTable extends React.Component<MyTableProps> {
+  constructor(props: MyTableProps) {
+    super(props);
+  }
+  render() {
+    const divList = this.props.humans.map((human) => <div>{human.name} ({human.age})</div>);
+    return (
+      <div>
+        {divList}
+      </div>
+    )
   }
 }
 
