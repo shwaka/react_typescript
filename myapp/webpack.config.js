72570f6915998edd31d32bce129869a2da922302
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2020-11-08 00:43:38 +0900

    remove development.js (merged to webpack.config.js)

--- myapp/webpack.config.js
+++ myapp/webpack.config.js
@@ -1,2 +1,54 @@
 require('@babel/register');
-module.exports = require('./development');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const dist = `${__dirname}/dist`
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.tsx',
+  output: {
+    path: dist,
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /\.(sa|sc|c)ss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { url: false }
+          },
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', '.js', '.json' ]
+  },
+  target: ['web', 'es5'],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    contentBase: dist,
+    watchContentBase: true,
+    inline: true,
+    hot: true
+    /* publicPath: '/',
+     * inline: true */
+  }
+};
